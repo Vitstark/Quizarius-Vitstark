801d8a6c6011a1bdbed97dab5e50e59bd3945bf3
Vitstark <vitstark@mail.ru>
2022-08-27 15:15:16 +0300

    init commit

---
+++ src/main/java/ru/vitstark/quizarius/QuizariusApplication.java
@@ -0,0 +1,13 @@
+package ru.vitstark.quizarius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizariusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizariusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vitstark/quizarius/QuizariusApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vitstark.quizarius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizariusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
