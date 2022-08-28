134450726f19e918b77e894038cc933865c605a4
Vitstark <vitstark@mail.ru>
2022-08-28 23:38:11 +0300

    test added

--- src/main/java/ru/vitstark/quizarius/controllers/MainController.java
+++ src/main/java/ru/vitstark/quizarius/controllers/MainController.java
@@ -8,7 +8,7 @@ public class MainController {
 
     @GetMapping("")
     public String toMainPageRedirect() {
-        return "redirect:main";
+        return "redirect:/main";
     }
 
     @GetMapping("/main")
---
+++ src/test/java/ru/vitstark/quizarius/controllers/MainControllerTest.java
@@ -0,0 +1,27 @@
+package ru.vitstark.quizarius.controllers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(MainController.class)
+@AutoConfigureMockMvc
+public class MainControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testMainPage() throws Exception {
+        mvc.perform(get("/main"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("main"))
+                .andExpect(content().string(
+                        containsString("Quizarius")));
+    }
+}
