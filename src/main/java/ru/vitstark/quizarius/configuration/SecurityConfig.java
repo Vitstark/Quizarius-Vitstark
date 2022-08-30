215d3cc8151cb355f68fcc3ac95bb0a4b87feece
Vitstark <vitstark@mail.ru>
2022-08-30 13:17:29 +0300

    csrf enabled, error table edited

--- src/main/java/ru/vitstark/quizarius/configuration/SecurityConfig.java
+++ src/main/java/ru/vitstark/quizarius/configuration/SecurityConfig.java
@@ -21,8 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/game/*").hasAnyRole("ADMIN", "USER")
                 .anyRequest().permitAll()
                 .and()
